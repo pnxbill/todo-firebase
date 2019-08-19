aef0cf31b2da8acbd9add060af935f95f6637376
pnxbill <pnxbill@gmail.com>
2019-08-19 10:11:32 -0300

    inicial setup

---
+++ src/components/containers/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Container } from './styles';
+
+function App() {
+  return (
+    <Container className='App'>
+      Something new is coming. A firebase app with auth.
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/components/containers/styles.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  display: flex;
+
+  flex: 1;
+  color: white;
+  /* vertical-align: center; */
+  /* align-items: center; */
+  justify-content: center;
+  height: 100%;
+
+  background-color: black;
+`;
